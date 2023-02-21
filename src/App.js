6fadefcaee1bb5b1116fb207047b75a65c9a3a47
2115-27 <103985748+2115-27@users.noreply.github.com>
2023-02-21 11:11:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          UPDATED CPQ DATA NON PROD JOB.
+          CPQ DATA NON PROD JOB TEST.
         </p>
       </header>
     </div>
@@ -12,22 +13,3 @@ function App() {
 }
 
 export default App;
-
-
-// import SignUp from "./components/SignUp";
-// import Login from "./components/Login";
-// import { Route, Switch, Redirect } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <>
-//       <Switch>
-//         <Route path="/login" component={Login} />
-//         <Route path="/signup" component={SignUp} />
-//         <Redirect from="/" to="/signup" />
-//       </Switch>
-//     </>
-//   );
-// }
-
-// export default App;
